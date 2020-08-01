ed31ac630aa9f5f52557c3f04a8244234537001d
ottagit <ottacc66@gmail.com>
2020-08-01 13:01:09 +0300

    basic number and string methods

---
+++ numbers.js
@@ -0,0 +1,34 @@
+var a;
+var b;
+
+a = 10;
+b = 5;
+var sum;
+
+sum = a+b;
+// print out the value of sum on the default console
+/* on line 11 we call the log method on the console object
+  and pass sum as the argument to the log method 
+  the dot (.) oparator is used when calling (inoking) a method
+  or a functon */
+console.log('The value of sum is', sum)
+
+var c, d;
+c = 7,d = 4;
+
+// multiply c and d
+var product = c * d;
+console.log('The product of c and d is', product);
+var x = 6;
+var div;
+div = c / d;
+var sub;
+sub = d - a;
+product = a * b;
+sum = x + c;
+var mod;
+mod = c % b;
+product = x * c;
+console.log('the modulo of c and b is', mod);
+console.log('the dividend of c and d is', div);
+console.log('the subtraction of d and a is', sub);
---
+++ strings.js
@@ -0,0 +1,13 @@
+// string munipulation using methods
+
+// declare a string object
+var name = 'lewis';
+// capitalize the entire string object
+var capital = name.toUpperCase();
+console.log('converting name to capital gives', capital);
+var name2 = 'LEWIS';
+var lower = name2.toLowerCase();
+console.log('converting name2 to lower case gives', lower);
+// indexing in strings.
+var first_index = name.indexOf('l');
+console.log('the index of the first character of the string object name is', first_index);
